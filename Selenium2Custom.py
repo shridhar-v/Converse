ed687bb464d4777ee04c151268d0343c64080dc8
shridhar-v <shridharv@fidelis-inc.com>
2014-06-25 22:42:08 +0530

    Updated Python Libs

--- Selenium2Custom.py
+++ Selenium2Custom.py
@@ -34,5 +34,22 @@ class Selenium2Custom(Selenium2Library):
     def switch_to_last_window(self):             
         handles = self._current_browser().get_window_handles()
         self._current_browser().switch_to_window(handles[-1])
+    
+    
+    def drag_drop_with_offset_to_element(self, locator_source, locator_target, x_by_10=1, y_by_10=1):
+        
+        source = self._element_find(locator_source, True, False)
+        target = self._element_find(locator_target, True, False)
+        
+        p1 = source.location
+        p2 = target.location
+        d2 = target.size
+                    
+        x_offset = 10 #(d2['width']/10)*x_by_10
+        y_offset = 10 #(d2['height']/10)*y_by_10
+        
+        offset = {'x':p2['x']-p1['x'],'y':p2['y']-p1['y']}
+        self.drag_and_drop_by_offset(locator_source, offset['x']+x_offset, offset['y']+y_offset)         
+        
           
         
